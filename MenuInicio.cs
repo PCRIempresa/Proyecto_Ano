5d06a8679c8077d05fff20cc04724cc6758c04b2
Marcos <marcos191196@hotmail.com>
2023-09-26 22:07:05 -0300

    primer commit

---
+++ MenuInicio.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_Año
+{
+    public partial class MenuInicio : Form
+    {
+        public MenuInicio()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void ImagenFondo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private class Public
+        {
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelRegistrate_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
